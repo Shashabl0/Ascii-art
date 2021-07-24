1716056fca8a2752450d4c1d02175b5bffecf06e
Shashank Barnwal <shashank.barnwal1999@gmail.com>
2021-07-24 18:47:13 +0530

    update index

--- index.py
+++ index.py
@@ -45,4 +45,4 @@ def display_image(filename):
 	return redirect(url_for('static', filename='/' + 'outImage.png'), code=301)
 
 if __name__ == "__main__":
-    app.run(debug=True,host=('0.0.0.0'))
+    app.run(debug=False,host=('0.0.0.0'))
